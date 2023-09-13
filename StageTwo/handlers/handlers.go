a53f8db16e07c9c6c97fa4c28c0a2d9699473879
DavidHODs <davidoluwatobi41@gmail.com>
2023-09-13 05:53:21 +0100

    implemented logic for creating and storing person record

--- StageTwo/handlers/handlers.go
+++ StageTwo/handlers/handlers.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/DavidHODs/HNGx/StageTwo/db"
+)
 
 // Person data structure with assigned struct tags
 type Person struct {
@@ -17,4 +24,38 @@ type Response struct {
 	Msg    string `json:"msg"`
 	Data   string `json:"data"`
 	Status int    `json:"status"`
+}
+
+// CreatePerson creates a new person and add them to the database
+func CreatePerson(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	var person Person
+
+	// decodes json data from request body, parses it and stores it in person variable
+	err := json.NewDecoder(req.Body).Decode(&person)
+	if err != nil {
+		http.Error(res, "error: bad request", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Println(person.FullName)
+
+	// SQL query to insert a record; avouds SQL injection
+	query := `INSERT INTO person (fullName, isDeleted) 
+			  VALUES ($1, $2)
+			  RETURNING id`
+
+	// executes the query and returns the id of the created person
+	err = db.DB.QueryRow(query, person.FullName, false).Scan(&person.ID)
+	if err != nil {
+		http.Error(res, fmt.Sprintf("error: failed to insert record into database ==> %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	// Return the created person as JSON
+	json.NewEncoder(res).Encode(Response{
+		Msg:    "record succesfully created",
+		Data:   person.FullName,
+		Status: http.StatusCreated,
+	})
 }
