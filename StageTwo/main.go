1f38a095d878c86815aa07b2f14f100638c03738
DavidHODs <davidoluwatobi41@gmail.com>
2023-09-14 14:21:52 +0100

    lazy networking for aws server

--- StageTwo/main.go
+++ StageTwo/main.go
@@ -48,7 +48,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler:      router,
-		Addr:         fmt.Sprintf("127.0.0.1:%d", port),
+		Addr:         fmt.Sprintf("0.0.0.0:%d", port),
 		WriteTimeout: 10 * time.Second,
 		ReadTimeout:  10 * time.Second,
 	}
